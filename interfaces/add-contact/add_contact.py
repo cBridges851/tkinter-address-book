ba5ee8212a2837cc4f242c0d79f0f3c2a619ce36
Christa Bridges <christa@pebblepad.co.uk>
2021-01-02 12:27:03 +0000

    interface-design All labels are now displayed on the add_contact window.

--- interfaces/add-contact/add_contact.py
+++ interfaces/add-contact/add_contact.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, mainloop, Button
+from tkinter import Tk, mainloop, Button, Label
 from PIL import Image, ImageTk
 
 class AddContact:
@@ -11,9 +11,30 @@ class AddContact:
         self.root.iconbitmap("../favicon.ico")
         self.root.resizable(False, False)
         
-        back_button_image = ImageTk.PhotoImage(Image.open("../Back.png").resize((250,125)), Image.ANTIALIAS)
+        back_button_image = ImageTk.PhotoImage(Image.open("../Back.png").resize((70,38)), Image.ANTIALIAS)
         back_button = Button(self.root, image=back_button_image)
         back_button.grid(row=0, column=0)
+
+        labels = [
+            "Forename", 
+            "Surname", 
+            "Address Line 1", 
+            "Address Line 2", 
+            "Town/City", 
+            "County", 
+            "Country", 
+            "Postcode", 
+            "Phone Number", 
+            "Email Address"
+        ]
+
+        row_count = 1
+        for label in labels:
+            current_label = Label(self.root, text=f"{label}:", font=("Helvatica 14"))
+            current_label.grid(row=row_count, column=1)
+            row_count += 1
+
+
         self.root.mainloop()
 
 AddContact().render()
